191a0ba9aad7dfae5a6fa093d86a9194e25bc5ce
DarWafa <wafahssain@gmail.com>
2017-08-11 11:54:15 +0300

    Dynamic URL's patterns

--- coded_app/urls.py
+++ coded_app/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from posts import views
+from coded_app import views
 
 urlpatterns = [
     url(r'^create/$', views.post_create, name="create"),
-    url(r'^detail/$', views.post_detail, name="detail"),
+    url(r'^detail/(?P<post_id>\d+)/$', views.post_detail, name="detail"),
     url(r'^list/$', views.post_list, name="list"),
     url(r'^update/$', views.post_update, name="update"),
     url(r'^delete/$', views.post_delete, name="delete"),
